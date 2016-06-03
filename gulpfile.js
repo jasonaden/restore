492ba3b0f3ce7fe31ec3aca1a9d42011431392ef
jacobmumm <jacob.mumm@gmail.com>
2016-06-03 12:34:15 -0400

    amd module js with dts

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,45 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var ts = require('gulp-typescript');
+var tsc = require('gulp-typescript');
 var merge = require('merge2');
+var tsProject = tsc.createProject('tsconfig.json');
 
 gulp.task('default', ['compile', 'concat-tsd']);
 
 gulp.task('compile', function(){
-  var tsResult = gulp.src('./src/**/*.ts')
+  var tsAmd = gulp.src('./src/**/*.ts')
+    .pipe(tsc(tsProject));
+  tsAmd.js.pipe(gulp.dest('./dist'));
+  
+  var tsCommon = gulp.src('./src/**/*.ts')
+    .pipe(tsc({
+      "target": "es5",
+      "module": "commonjs",
+      "declaration": true,
+      "outDir": "tsd"
+    }));
+  return tsCommon.dts.pipe(gulp.dest('./tsd'));
+});
+
+gulp.task('old-compile', function(){
+  var tsAmd = gulp.src('./src/**/*.ts')
       .pipe(ts({
-        declaration: true, 
-        noExternalResolve: true
+        "target": "es5",
+        "module": "amd",
+        "outFile": "dist/index.js"
       }));
 
+  var tsCommon = gulp.src('./src/**/*.ts')
+    .pipe(ts({
+      "target": "es5",
+      "module": "commonjs",
+      "declaration": true,
+      "outDir": "out"
+    }));
+    
     return merge([
-      tsResult.dts.pipe(gulp.dest('out/tsd')),
-      tsResult.js.pipe(gulp.dest('dist/'))
+      tsCommon.dts.pipe(gulp.dest('out/tsd')),
+      tsAmd.js.pipe(gulp.dest('dist'))
     ]);
 });
 
@@ -25,7 +50,7 @@ gulp.task('concat-js', ['compile'], function(){
 });
 
 gulp.task('concat-tsd', ['compile'], function(){
-  return gulp.src('./out/tsd/**/*.d.ts')
+  return gulp.src('./tsd/**/*.d.ts')
     .pipe(concat('index.d.ts'))
     .pipe(gulp.dest('./dist/'));
 });
